60c46356a0b70f17eaa540a1b434587e7d003003
Sean Boots <sean.boots@gmail.com>
2020-05-03 14:18:17 -0700

    Show/hide all report images

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,11 @@
 
 var app = app || {};
 
+app.text = {
+  showImage: "Show image",
+  hideImage: "Hide image",
+};
+
 app.toggleImage = function (targetId) {
   var elem = $("#" + targetId);
   var isVisible = $(elem).data("target-visible") == "1";
@@ -10,17 +15,30 @@ app.toggleImage = function (targetId) {
   if (isVisible) {
     // Hide the image
     $("#" + targetId + " .irt-image-img").hide();
-    $("#" + targetId + " .irt-toggle-image").text("Show image");
+    $("#" + targetId + " .irt-toggle-image").text(app.text.showImage);
     $(elem).data("target-visible", "0");
   } else {
     // Show the image
     $("#" + targetId + " .irt-image-img").show();
-    $("#" + targetId + " .irt-toggle-image").text("Hide image");
+    $("#" + targetId + " .irt-toggle-image").text(app.text.hideImage);
     $(elem).data("target-visible", "1");
   }
 };
 
+app.hideAllImages = function () {
+  $(".irt-image .irt-image-img").hide();
+  $(".irt-image .irt-toggle-image").text(app.text.showImage);
+  $(".irt-image").data("target-visible", "0");
+};
+
+app.showAllImages = function () {
+  $(".irt-image .irt-image-img").show();
+  $(".irt-image .irt-toggle-image").text(app.text.hideImage);
+  $(".irt-image").data("target-visible", "1");
+};
+
 $(function () {
+  // Button click assignments:
   $(".irt-print-current").on("click", function (e) {
     window.print();
   });
@@ -29,5 +47,13 @@ $(function () {
     var elem = $(e.currentTarget);
     var target = $(elem).data("target-img");
     app.toggleImage(target);
+  });
+
+  $(".irt-hide-all-images").on("click", function (e) {
+    app.hideAllImages();
+  });
+
+  $(".irt-show-all-images").on("click", function (e) {
+    app.showAllImages();
   });
 });
